0dc863982056bf8048803d14b61be1b40a670621
unknown <phetaibtc@gmail.com>
2025-04-26 08:49:25 +0700

    uppp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   ssr: true,
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  runtimeConfig: {
+    public: {
+      API_URL: process.env.API_URL
+    }
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+        <a-button type="primary">Primary Button</a-button>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
